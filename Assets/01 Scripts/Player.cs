bffd624d00e3094bd6568436dee63781fd29aac4
HEROWL <hiroakitogawa74@gmail.com>
2024-06-13 13:47:20 +0900

    togawa made

--- Assets/01 Scripts/Player.cs
+++ Assets/01 Scripts/Player.cs
@@ -1,69 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
+    private float moveSpeed = 5f;
     public float jumpForce = 10f;
-    public float moveSpeed = 10f;
-    private bool detectedLeft;
-    private bool detectedRight;
-    [SerializeField] private ObjectSensor sensorG;
-    [SerializeField] private ObjectSensor sensorLT;
-    [SerializeField] private ObjectSensor sensorLB;
-    [SerializeField] private ObjectSensor sensorRT;
-    [SerializeField] private ObjectSensor sensorRB;
+    private bool isGrounded;
 
-    Rigidbody2D rb;
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb;
+    private Collider2D col;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        DetectedOnRight();
-        DetectedOnLeft();
-
-        float inputX = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(inputX * moveSpeed, rb.velocity.y);
-        if (sensorG.dectected && Input.GetKeyDown(KeyCode.Space) && !detectedLeft && !detectedRight) 
-        {
-            Jump();
-        }
+        Move();
+        Jump();
     }
 
-    
+    private void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveX, 0f, 0f);
+        transform.position += movement * moveSpeed * Time.deltaTime;
+    }
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-    }
-
-    private void DetectedOnLeft() 
-    {
-        if (sensorLT.dectected && sensorLB.dectected)
+        if(Input.GetButtonDown("Jump") && isGrounded)
         {
-            detectedLeft = true;
-        }
-        else
-        {
-            detectedLeft = false;
-        }
-    }
-    private void DetectedOnRight()
-    {
-        if (sensorRT.dectected && sensorRB.dectected)
-        {
-            detectedRight = true;
-        }
-        else
-        {
-            detectedRight = false;
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+           isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+    
+
+    
 }
+
