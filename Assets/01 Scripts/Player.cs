0de1cf92ac6fe64da73ef2d0b0d2a8634652a2f5
Hiroaki Togawa <hiroakitogawa74@gmail.com>
2024-07-14 21:41:41 +0900

    commit

--- Assets/01 Scripts/Player.cs
+++ Assets/01 Scripts/Player.cs
@@ -1,106 +1,188 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
-    private float moveSpeed = 5f;
-    public float jumpForce = 10f;
-    private bool isGrounded;
-    private bool isTouchingWall;
-    private bool isWallOnRight; // 壁が右にあるかどうか
+    GameController gameController;
+    [SerializeField] private float jumpForce = 45f;
+    [SerializeField] private float moveSpeed = 30f;
+    [SerializeField] private float triangleJumpForce = 15f;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    AudioClip walkingSound;
 
-    private Rigidbody2D rb;
-    private Collider2D col;
+    private bool detectedLeft;
+    private bool detectedRight;
+    private bool onMove;
+    private bool isMove;
+    private bool onGround;
+    private bool canTriangleJump;
 
-    private void Start()
+    private ObjectSensor sensorLT;
+    private ObjectSensor sensorLB;
+    private ObjectSensor sensorRT;
+    private ObjectSensor sensorRB;
+
+    [SerializeField] private Vector2 angleLT = new Vector2(-1, 3);
+    [SerializeField] private Vector2 angleRT = new Vector2(1, 3);
+
+    Rigidbody2D rb;
+
+    void Start()
     {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //SensorLeftTop, 左上
+        sensorLT = transform.Find("Sensor_LT").GetComponent<ObjectSensor>();
+        //LeftBottom, 左下
+        sensorLB = transform.Find("Sensor_LB").GetComponent<ObjectSensor>();
+        //Right Top, 右上
+        sensorRT = transform.Find("Sensor_RT").GetComponent<ObjectSensor>();
+        //Right Bottom, 右下
+        sensorRB = transform.Find("Sensor_RB").GetComponent<ObjectSensor>();
+
+        gameController = FindObjectOfType<GameController>().GetComponent<GameController>();
+        if (gameController == null)
+        {
+            Debug.Log("Player: Cannot Find GameContorller");
+        }
+        else
+        {
+            Debug.Log("Player: Successfully find GameContorller");
+        }
+
         rb = GetComponent<Rigidbody2D>();
-        col = GetComponent<Collider2D>();
+        onMove = true;
     }
 
+    // Update is called once per frame
     void Update()
     {
-        Move();
-        Jump();
-    }
-
-    private void Move()
-    {
-        float moveX = Input.GetAxis("Horizontal");
-
-        Vector3 movement = new Vector3(moveX, 0f, 0f);
-        transform.position += movement * moveSpeed * Time.deltaTime;
-    }
-
-    private void Jump()
-    {
-        if (Input.GetButtonDown("Jump") && (isGrounded || isTouchingWall))
+        animator.SetBool("IsMove", isMove);
+        if (gameController.onGame)
         {
-            // 壁キックロジック
-            if (isTouchingWall && !isGrounded)
+            DetectedOnRight();
+            DetectedOnLeft();
+            //Move
+            if (onMove && Input.GetKey(KeyCode.A) && !detectedLeft)
             {
-                // 壁キックの方向を設定
-                Vector2 wallKickDirection;
-                if (isWallOnRight)
-                {
-                    wallKickDirection = new Vector2(-1, 1).normalized; // 左斜め上方向
-                }
-                else
-                {
-                    wallKickDirection = new Vector2(1, 1).normalized; // 右斜め上方向
-                }
-                rb.AddForce(wallKickDirection * jumpForce, ForceMode2D.Impulse);
-
-                // 壁キック後のクールダウンを設定
-                StartCoroutine(WallJumpCooldown());
+                //移動を押した時加速を初期化(三角飛び中など)
+                rb.velocity = new Vector2(0, rb.velocity.y);
+                spriteRenderer.flipX = false;
+                transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+                isMove = true;
             }
-            else
+            else if (onMove && Input.GetKey(KeyCode.D) && !detectedRight)
             {
-                // 通常のジャンプ
-                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                rb.velocity = new Vector2(0, rb.velocity.y);
+                spriteRenderer.flipX = true;
+                transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+                isMove = true;
+            }
+            else 
+            {
+                isMove = false;
+            }
+            //Jump
+            if (Input.GetKeyDown(KeyCode.Space) && onGround)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }//Triangle Jump
+            else if (canTriangleJump && Input.GetKeyDown(KeyCode.Space) && !onGround && detectedLeft && !detectedRight)
+            {
+                rb.velocity = angleRT * triangleJumpForce;
+                StartCoroutine(WaitTriangleJump());
+            }
+            else if (canTriangleJump && Input.GetKeyDown(KeyCode.Space) && !onGround && !detectedLeft && detectedRight)
+            {
+                rb.velocity = angleLT * triangleJumpForce;
+                StartCoroutine(WaitTriangleJump());
             }
         }
+        else if (!gameController.gameClear)
+        {
+            this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            this.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        else if (gameController.gameClear)
+        {
+            this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            this.GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+
     }
 
-    private IEnumerator WallJumpCooldown()
+    //三角飛び後すぐの方向転換を防ぐ
+    IEnumerator WaitTriangleJump()
     {
-        isTouchingWall = false; // 壁キック後、すぐに再び壁に触れた状態にしない
-        yield return new WaitForSeconds(0.2f); // クールダウン時間
-        isTouchingWall = true;
+        onMove = false;
+        yield return new WaitForSecondsRealtime(0.3f);
+        onMove = true;
     }
 
+    //左上と左下を感知した時活性化、これにより左が完全に触れた時にだけ三角飛び可
+    private void DetectedOnLeft()
+    {
+        if (sensorLT.dectected && sensorLB.dectected)
+        {
+            Debug.Log("Left is dectected!");
+            detectedLeft = true;
+        }
+        else
+        {
+            Debug.Log("Left is outed!");
+            detectedLeft = false;
+        }
+    }
+    //右上と右下を感知した時活性化
+    private void DetectedOnRight()
+    {
+        if (sensorRT.dectected && sensorRB.dectected)
+        {
+            Debug.Log("Right is dectected!");
+            detectedRight = true;
+        }
+        else
+        {
+            Debug.Log("Right is outed!");
+            detectedRight = false;
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        //足場か床に着いたとき止まらせるため（これがないと三角飛び後も滑ってしまう）
+        //Scaffoldは足場、足場だけの何かを作る可能性があるため分けています
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Scaffold"))
         {
-            isGrounded = true;
-        }
-        if (collision.gameObject.CompareTag("Wall"))
-        {
-            isTouchingWall = true;
-
-            // 壁の左右を判別
-            if (collision.contacts[0].point.x > transform.position.x)
-            {
-                isWallOnRight = true; // 壁が右にある
-            }
-            else
-            {
-                isWallOnRight = false; // 壁が左にある
-            }
+            Debug.Log("Ground!");
+            rb.velocity = new Vector2(0, 0);
         }
     }
-
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.CompareTag("Scaffold"))
         {
-            isGrounded = false;
+            onGround = true;
         }
-        if (collision.gameObject.CompareTag("Wall"))
+        //左右センサー感知だけではくっついてないのに三脚飛びができてしまうためCollisionを確認
+        if (collision.gameObject.tag == "Wall")
         {
-            isTouchingWall = false;
+            canTriangleJump = true;
         }
     }
+    private void OnCollisionExit2D(Collision2D collision) 
+    {
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.CompareTag("Scaffold"))
+        {
+            onGround = false;
+        }
+        if (collision.gameObject.tag == "Wall")
+        {
+            canTriangleJump = false;
+        }
+    }
+
 }
---
+++ Assets/01 Scripts/Title/TitileManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitileManager : MonoBehaviour
+{
+    bool isControl;
+    [SerializeField] GameObject titleObjects;
+    [SerializeField] GameObject howToControl;
+    bool onHowToControl;
+    [SerializeField] GameObject credits;
+    bool onCredits;
+    // Start is called before the first frame update
+    void Start()
+    {
+        onHowToControl = false;
+        onCredits = false;
+        howToControl.SetActive(false);
+        credits.SetActive(false);
+        StartCoroutine(WaitLoading());
+    }
+    private void Update()
+    {
+        if (isControl && !onCredits && Input.GetKeyDown(KeyCode.Space))
+        {
+            ShowHowToPlay();
+        }
+        else if (isControl && !onHowToControl && Input.GetKeyDown(KeyCode.C))
+        {
+            ShowCredit();
+        }
+        else if (isControl && Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("Stage01");
+        }
+    }
+    void ShowHowToPlay()
+    {
+        if (!onHowToControl)
+        {
+            onHowToControl = true;
+            titleObjects.SetActive(false);
+            howToControl.SetActive(true);
+        }
+        else 
+        {
+            onHowToControl = false;
+            howToControl.SetActive(false);
+            titleObjects.SetActive(true);
+        }
+    }
+    void ShowCredit()
+    {
+        if (!onCredits)
+        {
+            onCredits = true;
+            titleObjects.SetActive(false);
+            credits.SetActive(true);
+        }
+        else
+        {
+            onCredits = false;
+            credits.SetActive(false);
+            titleObjects.SetActive(true);
+        }
+    }
+    private IEnumerator WaitLoading() 
+    {
+        yield return new WaitForSeconds(3);
+        isControl = true;
+        StopCoroutine(WaitLoading());
+    }
+
+}
